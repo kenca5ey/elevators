a574e8bad3c3c7d0b619b7efea9c56b3354e829a
Casey <kcasey@deloitte.ie>
2016-10-18 00:02:28 +0100

    Better logic for using idle elevators

--- js/liftmaster.js
+++ js/liftmaster.js
@@ -13,12 +13,21 @@ var LiftMaster = (function (LiftMaster) {
     LiftMaster.prototype.addRequest = function (floorNumber, mentality) {
 
         this.lifts.sort(function (a, b) {
-            return Math.abs(a.getCurrentFloor() - floorNumber) - Math.abs(b.getCurrentFloor() - floorNumber);
+            var numJobsA = a.getStatus().downwardDestinations.length + a.getStatus().upwardDestinations.length,
+                numJobsB = b.getStatus().downwardDestinations.length + b.getStatus().upwardDestinations.length;
+            if (numJobsA < numJobsB) {
+                return -1;
+            } else if (numJobsA > numJobsB) {
+                return 1;
+            } else {
+                return Math.abs(a.getCurrentFloor() - floorNumber) - Math.abs(b.getCurrentFloor() - floorNumber);
+            }
         });
 
         this.lifts[0].addDestination(floorNumber, null);
 
-        //@todo build logic for selecting the best placed elevator based on criteria
+        //@todo refine logic for selecting the best placed elevator on pushing floor buttons
+        //@todo Fix bug - At the moment the elevator will stop for down/up requests on the way up/down
     };
 
     return LiftMaster;
